0a5fe93ec64370522e245921e96923b1f168672f
Icare741 <94448198+Icare741@users.noreply.github.com>
2022-11-21 14:13:17 +0100

    ✅add limite rate 1000 per minute

--- Server/routes/api.php
+++ Server/routes/api.php
@@ -25,12 +25,18 @@ Route::get('/user/{id}', [UserController::class, 'show']);
 Route::post('/login', [UserController::class, 'login']);
 Route::post('/register', [UserController::class, 'register']);
 
+//Test 
+Route::middleware('auth:api')->get('/test', function(){
+    return "hello world";
+});
+
+
 //Post
 Route::middleware('auth:api')->get('/post', [PostController::class, 'index']);
 Route::middleware('auth:api')->post('/post', [PostController::class, 'store']);
 Route::middleware('auth:api')->delete('/post/{id}', [PostController::class, 'destroy']);
 
-Route::group(['middleware' => ['auth:api', 'throttle:perso']], function() {
+Route::group(['middleware' => ['auth:api']], function() {
     
     Route::get('/user', [UserController::class, 'index']);
   });
